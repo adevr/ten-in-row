4f53b0d11906f8e33104e85e1d413458fce00d2c
Ubuntu <diogobarbosa1@outlook.pt>
2020-11-08 18:52:38 +0000

    nomenclature corrections

--- application/main.c
+++ application/main.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "Jogo.h"
 
 #define GAME_PID getpid()
@@ -13,7 +14,7 @@ void welcomeMenu() {
     printf("\nO 10-em-linha é um jogo conhecido da nossa infancia que consiste em deixar cair moedinhas\n"
            "de modo a formar uma linha no tabuleiro.");
     printf("\nEste jogo é baseado num sistema de pontuação: quantas mais linhas forem feitas com o mesmo caracter, maior será a pontuação final.");
-    printf("\nExistem 2 tipos de caracteres: X e O");
+    printf("\nExistem 2 tipos de caracteres: X e O.");
     printf("\nCada caracter será lançado à vez numa coluna escolhida pelo utilizador.");
     printf("\nO jogo termina quando o tempo chegar ao fim.\n\n");
 }
@@ -24,19 +25,21 @@ int main(int argc, char *argv[]) {
 
     welcomeMenu();
 
-    Jogo *jogo = criaJogo(GAME_PID);
-    iniciaJogo(jogo);
+    Game *game = createGame(GAME_PID);
+    initGame(game);
 
     while (1) {
         char pieceToPlay = (playsCounter % 2 == 0) ? PIECE_O : PIECE_X;
-        apresentaTabuleiro(jogo);
 
-        printf("\nPeça em jogo %c. Jogar na coluna: ", pieceToPlay);
+        apresentaTabuleiro(game);
+
+        printf("\nPeça em jogo %c. Jogar na coluna (1 - %i): ", pieceToPlay, NR_OF_COLUMNS);
         scanf("%i", &column);
 
-        executaJogada(jogo, pieceToPlay,column - 1);
+        executaJogada(game, pieceToPlay,column - 1);
 
         playsCounter ++;
+        system("clear");
     }
     return 0;
 }
